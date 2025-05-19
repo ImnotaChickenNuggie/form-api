048afd7cb22e5aee5e51fd9794245c103041d14f
Bruno <brunohdzv@gmail.com>
2025-05-18 21:11:17 -0600

    Feat: Ajustes para diseño css y tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 'use client';
-import Image from 'next/image';
 import { Button } from 'baseui/button';
 
 export default function Home() {
 	return (
 		<div>
 			<Button onClick={() => alert('click')}>Hello</Button>
+			<button className='bg-red-500'>holas</button>
+			<h1>sss</h1>
+			<h2>sss</h2>
 		</div>
 	);
 }
