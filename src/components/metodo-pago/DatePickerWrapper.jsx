962b968c7ae8ed89459d6b536683e6539361b91f
Bruno <brunohdzv@gmail.com>
2025-05-19 15:45:45 -0600

    Fix: Se corrige error de fecha

--- src/components/metodo-pago/DatePickerWrapper.jsx
+++ src/components/metodo-pago/DatePickerWrapper.jsx
@@ -3,19 +3,23 @@ import { Input } from "baseui/input";
 import { format } from 'date-fns';
 
 export function DatePickerWrapper({ value, onChange }) {
+
   const handleDateChange = (e) => {
-    const newDate = new Date(e.target.value);
-    onChange(newDate);
+    const isoDateString = e.target.value; 
+    onChange(isoDateString);
   };
 
+  const today = new Date();
+
   // Formatear la fecha para el input type="date"
-  const formattedDate = value ? format(value, 'yyyy-MM-dd') : '';
+  const formattedDate = value ? format(new Date(value), 'yyyy-MM-dd') : '';
 
   return (
     <Input
       type="date"
       value={formattedDate}
       onChange={handleDateChange}
+      min={format(today, 'yyyy-MM-dd')}
       overrides={{
         Input: {
           style: {
